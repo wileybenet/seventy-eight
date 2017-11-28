0bf23121cef6289ef316be82d97d1103cde5a769
wiley <wiley@bmdware.com>
2017-11-27 22:31:43 -0800

    syncing tree

--- src/cli/commands/sync.table.js
+++ src/cli/commands/sync.table.js
@@ -1,8 +1,21 @@
-const { color } = require('../../utils');
+// const { color } = require('../../utils');
+const _ = require('lodash');
+const seventyEight = require('../../seventy.eight');
 
+const load = () => {};
+const getAllRelations = Model => {
+  const relations = Model.getSchema().filter(field => field.relation).map(relation => seventyEight.modelCache[relation]);
+  return relations.concat(_.flatMap(relations, getAllRelations));
+};
 
 module.exports = {
-  syncTable(name) {
-    
+  syncTable(modelName) {
+    console.log(`not implemented`);
+    // load model
+    // introspect Model.getSchema()
+    const Model = load(modelName);
+    const relations = getAllRelations(Model);
+    const tree = _.uniq(relations.reverse());
+    console.log(`Syncing ${modelName} and it's relations (${tree.map(n => n.$constructor.name)})`);
   },
 };
