87e1ebc19352aabcc8c961c8d4aee41fec95882c
wiley <wiley@bmdware.com>
2017-11-30 16:45:06 -0800

    setup bugs

--- spec/setup.js
+++ spec/setup.js
@@ -4,38 +4,51 @@ const path = require('path');
 const fs = require('fs');
 const q = require('q');
 const client = require('../src/lib/db.client');
-
-const args = [
-  '-u', process.env.DB_USER,
-  `-p${process.env.DB_PASSWORD}`,
-  '-h', process.env.DB_HOST,
-  '-P', process.env.DB_PORT,
-  '-e', `CREATE DATABASE IF NOT EXISTS ${process.env.DB_SCHEMA}`,
-];
-
-const createDB = spawn('mysql', args);
-console.log(`creating database ${process.env.DB_SCHEMA}`);
-
+const skipCreateDatabase = process.env.MYSQL_ROOT_PASSWORD;
 const deferred = q.defer();
 
-createDB.stderr.on('data', data => {
-  console.log(`create database stderr: ${data}`);
+const createDatabase = () => new Promise((resolve, reject) => {
+  const args = [
+    '-u', process.env.DB_USER,
+    `-p${process.env.DB_PASSWORD}`,
+    '-h', process.env.DB_HOST,
+    '-P', process.env.DB_PORT,
+    '-e', `CREATE DATABASE IF NOT EXISTS ${process.env.DB_SCHEMA}`,
+  ];
+  const createDB = spawn('mysql', args);
+  console.log(`creating database ${process.env.DB_SCHEMA}`);
+
+  createDB.stderr.on('data', data => {
+    console.log(`create database stderr: ${data}`);
+  });
+
+  createDB.on('close', code => {
+    if (code === 0) {
+      resolve();
+    } else {
+      console.log(`create database failed, error code ${code}`);
+      reject();
+    }
+  });
 });
 
-createDB.on('close', code => {
-  if (code === 0) {
-    const sql = fs.readFileSync(path.resolve(__dirname, 'seed.sql')).toString().replace(/\n/g, ''); // eslint-disable-line no-sync
+const seedDatabase = () => new Promise((resolve, reject) => {
+  const sql = fs.readFileSync(path.resolve(__dirname, 'seed.sql')).toString().replace(/\n/g, ''); // eslint-disable-line no-sync
 
-    client.query(sql).then(function() {
-      console.log('DATABASE SEEDED');
-      deferred.resolve(true);
-      client.close();
-    }, function(err) {
-      console.log(err);
-    });
-  } else {
-    console.log(`create database failed, error code ${code}`);
-  }
+  client.query(sql).then(function() {
+    console.log('DATABASE SEEDED');
+    resolve(true);
+    client.close();
+  }, function(err) {
+    console.log(err);
+    reject();
+  });
 });
+
+if (skipCreateDatabase) {
+  seedDatabase().then(deferred.resolve).catch(console.error);
+} else {
+  createDatabase().then(seedDatabase).then(deferred.resolve).catch(console.error);
+}
 
 module.exports = deferred.promise;
