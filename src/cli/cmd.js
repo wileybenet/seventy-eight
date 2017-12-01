2828031de4cd7c96e8d1fd8904b9f518652bfdbb
wiley <wiley@bmdware.com>
2017-11-30 23:26:41 -0800

    fixing promise

--- src/cli/cmd.js
+++ src/cli/cmd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 /* eslint-disable node/shebang */
-const seventyEight = require('../seventy.eight');
 const { color } = require('../utils');
 const [, , cmd, ...options] = process.argv;
 
@@ -12,17 +11,13 @@ const cmdOptions = {
 };
 
 const command = cmdOptions[cmd];
-const closeDB = seventyEight.db.close;
 
 if (command) {
   command(...options).then(msg => {
     console.log(msg);
-    closeDB();
   }).catch(msg => {
     console.error(msg);
-    closeDB();
   });
 } else {
   console.log(`${error('ERROR:')} unknown command '${cmd || '<empty>'}', options:\n${Object.keys(cmdOptions).join('\n')}`);
-  closeDB();
 }
